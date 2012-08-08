01328b33283e8b89ff52a652bc4dd32e27cb414a
Shantanu Kumar <kumar.shantanu@gmail.com>
2012-08-08 21:24:56 +0530

    add CLJS error-handler that throws exception

--- src-cljs/basil/cljs.cljs
+++ src-cljs/basil/cljs.cljs
@@ -1,6 +1,13 @@
 (ns basil.cljs
   (:require [cljs.reader :as reader]
-            [basil.slot  :as slot]))
+            [basil.slot  :as slot]
+            [basil.types :as types]))
 
 
 (def slot-compiler (slot/make-slot-compiler reader/read-string))
+
+
+(defn err-handler
+  "Error handler for ClojureScript that throws exception instead of error messages."
+  [text] {:pre [(types/error-text? text)]}
+  (throw (js/Error ^String (str (:text text)))))
